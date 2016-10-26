a8abe9568da22f80adc3f495f7f5a514c24c8e0b
valery <valery.fremaux@gmail.com>
2016-10-26 23:56:34 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -19,19 +19,18 @@
  *
  * @package     block_auditquiz_results
  * @category    blocks
- * @author      Valery Fremaux <valery.fremaux@gmail.com>
- * @copyright   2015 onwards Valery Fremaux (http://www.mylearningfactory.com)
+ * @author      Valery Fremaux (valery.fremaux@gmeil.com)
+ * @copyright   2015 onwards Valery Fremaux (valery.fremaux@gmeil.com)
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016013104;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2014110400;        // Requires this Moodle version
+$plugin->requires  = 2015050500;        // Requires this Moodle version
 $plugin->component = 'block_auditquiz_results'; // Full name of the plugin (used for diagnostics)
-$plugin->release = "2.8.0 Build(2016013104)";
-$plugin->maturity = MATURITY_BETA;
+$plugin->release = "2.9.0 Build(2016013104)";
 $plugin->dependencies = array('local_vflibs' => 2016013100);
 
-// Non moodle attributes.
-$plugin->codeincrement = '2.8.0000';
+// Non moodle attributes
+$plugin->codeincrement = '2.9.0001';
