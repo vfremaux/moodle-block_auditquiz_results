4babd110b800e0b527e6492bc78caedcd2c4732a
valery <valery.fremaux@gmail.com>
2018-06-12 15:36:39 +0200

    feed M35 branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# moodle-block_auditquiz_results
-A block that plays with quizzes to forge a knowledge pre-positionning activity
--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016013104;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2017110800;        // Requires this Moodle version.
+$plugin->requires  = 2018042700;        // Requires this Moodle version.
 $plugin->component = 'block_auditquiz_results'; // Full name of the plugin (used for diagnostics).
-$plugin->release = "3.4.0 Build(2016013104)";
+$plugin->release = "3.5.0 Build(2016013104)";
 $plugin->maturity = MATURITY_RC;
 $plugin->dependencies = array('local_vflibs' => 2016013100);
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.4.0002';
+$plugin->codeincrement = '3.5.0002';
