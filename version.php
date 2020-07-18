fba950fa9ce5f2c7454c3232e569471348fbfe09
valery <valery.fremaux@gmail.com>
2020-07-18 15:40:05 +0200

    Automated sync from SVN

--- version.php
+++ version.php
@@ -27,12 +27,12 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016013104;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2019051100;        // Requires this Moodle version.
+$plugin->requires  = 2018112800;        // Requires this Moodle version.
 $plugin->component = 'block_auditquiz_results'; // Full name of the plugin (used for diagnostics).
-$plugin->release = "3.7.0 Build(2016013104)";
+$plugin->release = "3.6.0 Build(2016013104)";
 $plugin->maturity = MATURITY_RC;
 $plugin->dependencies = array('local_vflibs' => 2016013100);
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.7.0002';
+$plugin->codeincrement = '3.6.0002';
 $plugin->privacy = 'dualrelease';
